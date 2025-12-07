5ecc800f04b2f9914adc9eab920849dc246682e3
Jeffrey Zhang <zhang.jeffr@northeastern.edu>
2025-12-07 16:44:14 -0500

    added streamlit UI for Student User Stories

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -38,9 +38,7 @@ def create_app():
     app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("MYSQL_ROOT_PASSWORD").strip()
     app.config["MYSQL_DATABASE_HOST"] = os.getenv("DB_HOST").strip()
     app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("DB_PORT").strip())
-    app.config["MYSQL_DATABASE_DB"] = os.getenv(
-        "DB_NAME"
-    ).strip()  # Change this to your DB name
+    os.getenv("DB_NAME", "husky-fix").strip()  # Change this to your DB name
 
     # Initialize the database object with the settings above.
     app.logger.info("current_app(): starting the database connection")
